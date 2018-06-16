e1861e087a7ca6bd93e13f337062d274c8d20167
tang <dingyitt@163.com>
2018-06-17 04:46:46 +0800

    增加分页功能

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,6 @@
 from django.views.generic import TemplateView, ListView, DetailView
+from django.views.generic.list import MultipleObjectMixin
+from django.views.generic.base import View
 
 from .models import Post, Category, Tag
 
@@ -9,14 +11,24 @@ class IndexView(ListView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['categorys'] = Category.objects.all()
+        context['tags'] = Tag.objects.all()
         return context
 
 
 class BlogListView(IndexView):
-    model = Post
+    queryset = Post.objects.all()
     template_name = 'blog/blog_list.html'
     context_object_name = 'blogs'
-    paginate_by = 10
+    paginate_by = 5
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        page_obj = context['page_obj']
+        page_range = context['paginator'].page_range
+        first = max(page_obj.number-2, 1)
+        last = min(page_obj.number+2, page_range[-1])
+        context['paginator'] = page_range[first:last+1]
+        return context
 
 
 class BlogDetailView(DetailView):
