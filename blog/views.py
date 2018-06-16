b8cbfa48bee73863ff73f875ecc2bbabbb1d263e
tang <dingyitt@163.com>
2018-06-17 05:54:54 +0800

    修改分页

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
-from django.views.generic import TemplateView, ListView, DetailView
-from django.views.generic.list import MultipleObjectMixin
-from django.views.generic.base import View
+from django.views.generic import ListView, DetailView
+# from django.views.generic.list import MultipleObjectMixin
+# from django.views.generic.base import View
 
 from .models import Post, Category, Tag
 
@@ -20,15 +20,6 @@ class BlogListView(IndexView):
     template_name = 'blog/blog_list.html'
     context_object_name = 'blogs'
     paginate_by = 5
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        page_obj = context['page_obj']
-        page_range = context['paginator'].page_range
-        first = max(page_obj.number-2, 1)
-        last = min(page_obj.number+2, page_range[-1])
-        context['paginator'] = page_range[first:last+1]
-        return context
 
 
 class BlogDetailView(DetailView):
