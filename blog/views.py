f24383463b6054232dbe6ee7b8e81da3c8bf6af4
tang <dingyitt@163.com>
2018-06-17 22:59:21 +0800

    优化了分页功能

--- blog/views.py
+++ blog/views.py
@@ -20,6 +20,23 @@ class BlogListView(IndexView):
     template_name = 'blog/blog_list.html'
     context_object_name = 'blogs'
     paginate_by = 5
+    paginate_orphans = 3
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        paginator, page, object_list, is_paginated  = self.paginate_queryset(self.queryset, self.paginate_by)
+        left = max(page.number-2, 2)
+        right = min(page.number+2, paginator.num_pages-1)
+        page_nums = list(paginator.page_range)[left-1:right]
+        if left > 2:
+            page_nums.insert(0, '...')
+        if right < paginator.num_pages - 1:
+            page_nums.append('...')
+        page_nums.insert(0, 1)
+        page_nums.append(paginator.num_pages)
+        context['page_nums'] = page_nums
+        return context
+
 
 
 class BlogDetailView(DetailView):
