5e45f2eafc9e95c9dda603ae4e247cec4ba0e6cd
tang <dingyitt@163.com>
2018-06-18 03:48:36 +0800

    完善工具侧边栏

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import BlogListView, BlogDetailView
+from .views import BlogListView, BlogDetailView, CategoryView
 
 
 urlpatterns = [
     path('', BlogListView.as_view(), name='blog_list'),
+    path('<category>/', CategoryView.as_view(), name='blog_category'),
     path('<int:pk>/', BlogDetailView.as_view(), name='blog_detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
+from django.shortcuts import get_object_or_404
 from django.views.generic import ListView, DetailView
-# from django.views.generic.list import MultipleObjectMixin
-# from django.views.generic.base import View
+from django.db.models import Count
 
 from .models import Post, Category, Tag
 
@@ -12,19 +12,32 @@ class IndexView(ListView):
         context = super().get_context_data(**kwargs)
         context['categorys'] = Category.objects.all()
         context['tags'] = Tag.objects.all()
+        blog_dates = Post.objects.dates('created_time', 'month', order='DESC')
+        blog_dates_dict = {}
+        for blog_date in blog_dates:
+            blog_count = Post.objects.filter(created_time__year=blog_date.year,
+                                             created_time__month=blog_date.month).count()
+            blog_dates_dict[blog_date] = blog_count
+        context['dates'] = blog_dates_dict
         return context
 
 
 class BlogListView(IndexView):
-    queryset = Post.objects.all()
     template_name = 'blog/blog_list.html'
     context_object_name = 'blogs'
     paginate_by = 5
     paginate_orphans = 3
 
+    def get_queryset(self):
+        queryset = Post.objects.all()
+        return queryset
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        paginator, page, object_list, is_paginated  = self.paginate_queryset(self.queryset, self.paginate_by)
+        # Returns a 4-tuple containing (paginator, page, object_list, is_paginated).
+        page_context = self.paginate_queryset(self.get_queryset(), self.paginate_by)
+        paginator = page_context[0]
+        page = page_context[1]
         left = max(page.number-2, 2)
         right = min(page.number+2, paginator.num_pages-1)
         page_nums = list(paginator.page_range)[left-1:right]
@@ -37,6 +50,12 @@ class BlogListView(IndexView):
         context['page_nums'] = page_nums
         return context
 
+
+class CategoryView(BlogListView):
+    
+    def get_queryset(self):
+        self.category = get_object_or_404(Category, name=self.kwargs['category'])
+        return Post.objects.filter(category=self.category)
 
 class BlogDetailView(DetailView):
     template_name = 'blog/blog_detail.html'
